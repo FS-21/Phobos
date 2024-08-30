5d4616dae513ba553fa41520efbec8748c91fc05
FS-21 <firestorm21@gmail.com>
2024-08-30 18:13:53 +0200

    Compilation fixes

--- src/Ext/Techno/Body.cpp
+++ src/Ext/Techno/Body.cpp
@@ -828,22 +828,6 @@ void TechnoExt::HandleStopTarNav(EventExt* event)
 	}
 }
 
-bool TechnoExt::IsValidTechno(TechnoClass* pTechno)
-{
-	if (!pTechno)
-		return false;
-
-	bool isValid = !pTechno->Dirty
-		&& ScriptExt::IsUnitAvailable(pTechno, true)
-		&& pTechno->Owner
-		&& (pTechno->WhatAmI() == AbstractType::Infantry
-			|| pTechno->WhatAmI() == AbstractType::Unit
-			|| pTechno->WhatAmI() == AbstractType::Building
-			|| pTechno->WhatAmI() == AbstractType::Aircraft);
-
-	return isValid;
-}
-
 // =============================
 // load / save
 
