d11b50e0c73aa520d96df956e648a5382a56b934
FS-21 <firestorm21@gmail.com>
2025-10-12 23:06:02 +0200

    Changed House conversion behaviour
    Instead of mentioning Houses now it should mention Countries

--- src/Ext/Script/Hooks.cpp
+++ src/Ext/Script/Hooks.cpp
@@ -170,10 +170,10 @@ DEFINE_HOOK(0x723CA1, TeamMissionClass_FillIn_StringsSupport, 0xB)
 				break;
 			case PhobosScripts::ChangeToHouseByID:
 				action = 20;
-				index = HouseClass::FindIndexByName(textArgument);
+				index = HouseTypeClass::FindIndexOfName(textArgument);
 
 				if (index < 0)
-					ScriptExt::Log("AI Scripts - TeamMissionClass_FillIn_StringsSupport: Invalid House [%s]\n", textArgument);
+					ScriptExt::Log("AI Scripts - TeamMissionClass_FillIn_StringsSupport: Invalid Country string [%s]\n", textArgument);
 				break;
 			case PhobosScripts::PlaySpeechByID:
 				action = static_cast<int>(PhobosScripts::PlaySpeech);
